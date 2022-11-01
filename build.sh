d2ec0b662a6bf0fd28083d08e3afc4539ab6530d
Caio Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2022-11-01 11:58:40 -0300

    remove poetry

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 python -m pip install --upgrade pip
 
-poetry install
-
+python -m pip install -r requirements.txt
 python -m pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
